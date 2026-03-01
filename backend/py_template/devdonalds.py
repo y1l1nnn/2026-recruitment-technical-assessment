f00b861d9f5586a700aeeaef69fee78bbf022096
Elaine Zhang <cmddm39@gmail.com>
2026-03-01 19:15:08 +1100

    add backend task from last year...shhh

--- backend/py_template/devdonalds.py
+++ backend/py_template/devdonalds.py
@@ -21,6 +21,11 @@ class Recipe(CookbookEntry):
 class Ingredient(CookbookEntry):
 	cook_time: int
 
+@dataclass 
+class RecipeSummary():
+	name: str
+	cookTime: int
+	ingredients: List[RequiredItem]
 
 # =============================================================================
 # ==== HTTP Endpoint Stubs ====================================================
@@ -28,7 +33,7 @@ class Ingredient(CookbookEntry):
 app = Flask(__name__)
 
 # Store your recipes here!
-cookbook = None
+cookbook = {}
 
 # Task 1 helper (don't touch)
 @app.route("/parse", methods=['POST'])
@@ -42,25 +47,122 @@ def parse():
 
 # [TASK 1] ====================================================================
 # Takes in a recipeName and returns it in a form that 
-def parse_handwriting(recipeName: str) -> Union[str | None]:
-	# TODO: implement me
-	return recipeName
+def parse_handwriting(recipeName: str) -> Union[str, None]:
+	
+	recipeName = re.sub(r"[_-]", ' ', recipeName) 
+	recipeName = re.sub(r"[^a-zA-Z\s]", '', recipeName)	
+	recipeName = re.sub(r"\s+", ' ', recipeName).strip()
+	recipeName = recipeName.title()
 
+	if recipeName: 
+		return recipeName
+	else: 
+		return None
 
 # [TASK 2] ====================================================================
 # Endpoint that adds a CookbookEntry to your magical cookbook
 @app.route('/entry', methods=['POST'])
 def create_entry():
-	# TODO: implement me
-	return 'not implemented', 500
+	entry_data = request.get_json()
+	global cookbook
 
+	entry_type = entry_data.get("type")
+	entry_name = entry_data.get("name")
+
+	if entry_type != "recipe" and entry_type != "ingredient":
+		return jsonify({"error": "invalid type"}), 400
+
+	if entry_name in cookbook:
+		return jsonify({"error": "entry names must be unique"}), 400
+
+	if entry_type == "ingredient":
+		cook_time = entry_data.get("cookTime")
+
+		if cook_time is None or not isinstance(cook_time, int) or cook_time < 0:
+			return jsonify({"error": "cookTime cannot be negative"}), 400
+
+		cookbook[entry_name] = Ingredient(name=entry_name, cook_time=cook_time)
+
+	if entry_type == "recipe":
+		required_items = entry_data.get("requiredItems")
+
+		req_item_names = set()
+		req_items = []
+		for item in required_items:
+			item_name = item.get("name")
+			item_quantity = item.get("quantity")
+
+			if item_quantity is None or not isinstance(item_quantity, int) or item_quantity <= 0:
+				return jsonify({"error": "requiredItems can only have one element per name"}), 400
+			
+			if item_name in req_item_names: 
+				return jsonify({"error": "requiredItems can only have one element per name"}), 400
+
+			req_item_names.add(item_name)
+			req_items.append(RequiredItem(name=item_name, quantity=item_quantity))
+
+		cookbook[entry_name] = Recipe(name=entry_name, required_items=req_items)
+	
+	return "", 200
 
 # [TASK 3] ====================================================================
+# recurses through recipes and adds up total cookTime 
+def recurse_recipes(item, summary, quantity):
+	global cookbook
+
+	curr_item = cookbook.get(item.name)
+	if curr_item is None: return False
+	
+	if isinstance(curr_item, Ingredient):
+		summary.cookTime += curr_item.cook_time * quantity 
+
+		existing_ingredient = None 
+		for ing in summary.ingredients:
+			if ing.name == item.name:
+				existing_ingredient = ing
+				break
+
+		if existing_ingredient: 
+			existing_ingredient.quantity += quantity
+		else:
+			addIngredient = RequiredItem(
+				name=item.name,
+				quantity=quantity,
+			)
+			summary.ingredients.append(addIngredient)
+	
+	if isinstance(curr_item, Recipe):
+		for sub_item in curr_item.required_items:
+			if not recurse_recipes(sub_item, summary, sub_item.quantity * quantity):
+				return False
+
+	return True
+
 # Endpoint that returns a summary of a recipe that corresponds to a query name
 @app.route('/summary', methods=['GET'])
 def summary():
-	# TODO: implement me
-	return 'not implemented', 500
+	recipe_name = request.args.get("name")
+	global cookbook
+
+	if not recipe_name or recipe_name not in cookbook:
+		return "recipe not found", 400
+
+	recipe = cookbook[recipe_name]
+
+	if not isinstance(recipe, Recipe):
+		return "recipe not found", 400
+
+	summary = RecipeSummary(
+		name=recipe_name,
+		cookTime=0,
+		ingredients=[]
+	)
+	for item in recipe.required_items:
+		if not recurse_recipes(item, summary, item.quantity):
+			return "recipe/ingredient not found", 400
+
+	return jsonify(summary.__dict__), 200
+	
 
 
 # =============================================================================
--- backend/ts_template/devdonalds.ts
+++ backend/ts_template/devdonalds.ts
@@ -19,6 +19,12 @@ interface ingredient extends cookbookEntry {
   cookTime: number;
 }
 
+interface recipeSummary {
+  name: string; 
+  cookTime: number; 
+  ingredients: requiredItem[];
+}
+
 // =============================================================================
 // ==== HTTP Endpoint Stubs ====================================================
 // =============================================================================
@@ -26,7 +32,8 @@ const app = express();
 app.use(express.json());
 
 // Store your recipes here!
-const cookbook: any = null;
+export const cookbook: (recipe | ingredient)[] = [];
+// module.exports = { cookbook };
 
 // Task 1 helper (don't touch)
 app.post("/parse", (req:Request, res:Response) => {
@@ -43,26 +50,127 @@ app.post("/parse", (req:Request, res:Response) => {
 });
 
 // [TASK 1] ====================================================================
-// Takes in a recipeName and returns it in a form that 
+// Takes in a recipeName and returns it in a form that is legible 
 const parse_handwriting = (recipeName: string): string | null => {
-  // TODO: implement me
-  return recipeName
+
+  let regex =  /[-_]/g;
+  let newRecipeName: string = recipeName.replace(regex, ' ');
+  regex = /[^a-z\s]/g;
+  newRecipeName = newRecipeName.toLowerCase().replace(regex, '');
+  regex = /\s+/g;
+  newRecipeName = newRecipeName.replace(regex, ' ').trim();
+  
+  let splitName = newRecipeName.split(' ');
+  let capitalisedName = splitName.map(word => word.charAt(0).toUpperCase() + word.slice(1));
+  newRecipeName = capitalisedName.join(' ');
+
+  if (newRecipeName.length > 0) return newRecipeName;
+  return null;
 }
 
 // [TASK 2] ====================================================================
 // Endpoint that adds a CookbookEntry to your magical cookbook
 app.post("/entry", (req:Request, res:Response) => {
-  // TODO: implement me
-  res.status(500).send("not yet implemented!")
-
+  const entry = req.body;
+  
+  if (entry.type !== "recipe" && entry.type !== "ingredient") {
+    res.status(400).send("invalid type");
+    return;
+  }
+  if (entry.type === "ingredient" && entry.cookTime < 0) {
+    res.status(400).send("cookTime cannot be negative");
+    return;
+  }
+  if (cookbook.some(e => e.name === entry.name)) {
+    res.status(400).send("entry names must be unique");
+    return;
+  }
+  if (entry.name === "recipe") {
+    const reqItems = new Set();
+    for (const item of entry.requiredItems) {
+      if (item.quantity <= 0 || reqItems.has(item.name)) {
+        res.status(400).send("requiredItems can only have one element per name");
+        return;
+      }
+      reqItems.add(item.name);
+    }
+  }
+  let newEntry: recipe | ingredient;
+  if (entry.type === "recipe") {
+    newEntry = {
+      name: entry.name,
+      type: "recipe",
+      requiredItems: entry.requiredItems
+    }
+  } else {
+    newEntry = {
+      name: entry.name,
+      type: "ingredient",
+      cookTime: entry.cookTime 
+    }
+  }
+  cookbook.push(newEntry);
+  res.status(200).send();
 });
 
 // [TASK 3] ====================================================================
 // Endpoint that returns a summary of a recipe that corresponds to a query name
-app.get("/summary", (req:Request, res:Request) => {
-  // TODO: implement me
-  res.status(500).send("not yet implemented!")
 
+function recurseRecipes(item:requiredItem, summary:recipeSummary, quantity:number) {
+
+  const currItem = cookbook.find(e => e.name === item.name);
+  if (currItem === undefined) return false;
+
+  if (currItem.type === "ingredient") {
+    // Add to total cooktime 
+    const currIngredient = currItem as ingredient;
+    summary.cookTime += currIngredient.cookTime * quantity;
+
+    // Add ingredient quantity to summary
+    const existingIngredient = summary.ingredients.find(i => i.name === item.name);
+
+    if (existingIngredient) {
+      existingIngredient.quantity += quantity;
+    } else {
+      const addIngredient: requiredItem = {
+        name: item.name,
+        quantity: quantity 
+      }
+      summary.ingredients.push(addIngredient);
+    }
+  }
+  
+  if (currItem.type === "recipe") {
+    const currRecipe = currItem as recipe;
+    for (const subItem of currRecipe.requiredItems) {
+      if (recurseRecipes(subItem, summary, subItem.quantity * quantity) === false) {
+        return false;
+      }
+    }
+  }
+  return true;
+}
+
+app.get("/summary", (req:Request, res:Response) => {
+  const recipeName = req.query.name as string;
+
+  const reqRecipe = cookbook.find(r => r.name === recipeName);
+  if (reqRecipe === undefined || reqRecipe.type === "ingredient") {
+    res.status(400).send("recipe not found");
+    return;
+  }
+  const summary: recipeSummary = {
+    name: recipeName,
+    cookTime: 0,
+    ingredients: []
+  }
+  for (let currItem of (reqRecipe as recipe).requiredItems) {
+    if (recurseRecipes(currItem, summary, currItem.quantity) === false) {
+      res.status(400).send("recipe/ingredient not found");
+      return;
+    }
+  }
+  res.status(200).send(summary);
 });
 
 // =============================================================================
